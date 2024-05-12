c864522fdc7eac760db0723afd5978a7c51c9334
JoshMcDonagh <jmmarland99@gmail.com>
2024-05-12 20:46:41 +0100

    Add the generation of the properties table to the agents classconstructor

--- include/Agents.h
+++ include/Agents.h
@@ -4,20 +4,24 @@
 
 #ifndef AGENTS_H
 #define AGENTS_H
-#include <string>
-#include <variant>
+
+#include <memory>
 
 #include "AgentAttributes.h"
 
 
 class Agents {
 public:
-    explicit Agents(const AgentAttributes& agentAttributes);
+    explicit Agents(int numOfAgents, std::unique_ptr<std::string[]> agentNames, const AgentAttributes& agentAttributes);
     AgentAttributes getAgentAttributes();
     void runTick();
 
 private:
+    int numOfAgents_m;
     AgentAttributes agentAttributes_m;
+    std::map<std::string, int> agentIndexes_m;
+    int propertiesTableSize_m;
+    std::unique_ptr<std::vector<std::variant<int, double, std::string, bool>>[]> propertiesTable_m;
 };
 
 
