a950f0da081d1a061335588cfabf755876c2cef0
JoshMcDonagh <jmmarland99@gmail.com>
2024-06-03 16:01:07 +0100

    Begin implementation of the model results constructor

--- include/ModelResults.h
+++ include/ModelResults.h
@@ -6,9 +6,44 @@
 #define MODEL_RESULTS_H
 
 
+#include <functional>
+#include <vector>
+#include <variant>
+#include <string>
+#include "MemoryMappedTable.h"
+
+using StoredType = std::variant<int, double, std::string, bool>;
+using MergeFunc = std::function<std::vector<StoredType>(std::string, std::vector<StoredType>, std::vector<StoredType>)>;
 
 class ModelResults {
+public:
+    ModelResults(int coreNumber,
+                 MergeFunc mergeAgentPhysiologyPropertyResultsWithModelResultsFunc,
+                 MergeFunc mergeAgentBehaviourPropertyResultsWithModelResultsFunc,
+                 MergeFunc mergeAgentPhysiologyEventResultsWithModelResultsFunc,
+                 MergeFunc mergeAgentBehaviourEventResultsWithModelResultsFunc,
+                 MergeFunc mergePhysiologyPropertyResultsFunc,
+                 MergeFunc mergeBehaviourPropertyResultsFunc,
+                 MergeFunc mergePhysiologyEventResultsFunc,
+                 MergeFunc mergeBehaviourEventResultsFunc);
 
+private:
+    std::string physiologyPropertyResultsFilename_Suffix_m = "model_physiology_property_results.dat";
+    std::string behaviourPropertyResultsFilename_Suffix_m = "model_behaviour_property_results.dat";
+    std::string physiologyEventResultsFilename_Suffix_m = "model_physiology_event_results.dat";
+    std::string behaviourEventResultsFilename_Suffix_m = "model_behaviour_event_results.dat";
+    std::string physiologyPropertyResultsFilename_m;
+    std::string behaviourPropertyResultsFilename_m;
+    std::string physiologyEventResultsFilename_m;
+    std::string behaviourEventResultsFilename_m;
+    MergeFunc mergeAgentPhysiologyPropertyResultsWithModelResultsFunc_m;
+    MergeFunc mergeAgentBehaviourPropertyResultsWithModelResultsFunc_m;
+    MergeFunc mergeAgentPhysiologyEventResultsWithModelResultsFunc_m;
+    MergeFunc mergeAgentBehaviourEventResultsWithModelResultsFunc_m;
+    MergeFunc mergePhysiologyPropertyResultsFunc_m;
+    MergeFunc mergeBehaviourPropertyResultsFunc_m;
+    MergeFunc mergePhysiologyEventResultsFunc_m;
+    MergeFunc mergeBehaviourEventResultsFunc_m;
 };
 
 
--- src/ModelResults.cpp
+++ src/ModelResults.cpp
@@ -2,4 +2,28 @@
 // Created by Joshua McDonagh on 09/05/2024.
 //
 
+#include <utility>
+
 #include "../include/ModelResults.h"
+
+ModelResults::ModelResults(int coreNumber, MergeFunc mergeAgentPhysiologyPropertyResultsWithModelResultsFunc,
+                           MergeFunc mergeAgentBehaviourPropertyResultsWithModelResultsFunc,
+                           MergeFunc mergeAgentPhysiologyEventResultsWithModelResultsFunc,
+                           MergeFunc mergeAgentBehaviourEventResultsWithModelResultsFunc,
+                           MergeFunc mergePhysiologyPropertyResultsFunc, MergeFunc mergeBehaviourPropertyResultsFunc,
+                           MergeFunc mergePhysiologyEventResultsFunc, MergeFunc mergeBehaviourEventResultsFunc) {
+    physiologyPropertyResultsFilename_m = std::to_string(coreNumber) + "_" + physiologyPropertyResultsFilename_Suffix_m;
+    behaviourPropertyResultsFilename_m = std::to_string(coreNumber) + "_" + behaviourPropertyResultsFilename_Suffix_m;
+    physiologyEventResultsFilename_m = std::to_string(coreNumber) + "_" + physiologyEventResultsFilename_Suffix_m;
+    behaviourEventResultsFilename_m = std::to_string(coreNumber) + "_" + behaviourEventResultsFilename_Suffix_m;
+
+    mergeAgentPhysiologyPropertyResultsWithModelResultsFunc_m = std::move(mergeAgentPhysiologyPropertyResultsWithModelResultsFunc);
+    mergeAgentBehaviourPropertyResultsWithModelResultsFunc_m = std::move(mergeAgentBehaviourPropertyResultsWithModelResultsFunc);
+    mergeAgentPhysiologyEventResultsWithModelResultsFunc_m = std::move(mergeAgentPhysiologyEventResultsWithModelResultsFunc);
+    mergeAgentBehaviourEventResultsWithModelResultsFunc_m = std::move(mergeAgentBehaviourEventResultsWithModelResultsFunc);
+
+    mergePhysiologyPropertyResultsFunc_m = std::move(mergePhysiologyPropertyResultsFunc);
+    mergeBehaviourPropertyResultsFunc_m = std::move(mergeBehaviourPropertyResultsFunc);
+    mergePhysiologyEventResultsFunc_m = std::move(mergePhysiologyEventResultsFunc);
+    mergeBehaviourEventResultsFunc_m = std::move(mergeBehaviourEventResultsFunc);
+}
