5d04b72f4fea97b6b9c92e223c3fb8d6fe16743a
Joshua Mcdonagh <joshua.mcdonagh@postgrad.manchester.ac.uk>
2024-06-05 14:57:24 +0100

    Fix CMakeLists.txt

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/Home Desktop Files/Work/University of Manchester/PhD Work/Gitlab Work/Epydex/Epydex
+# Install script for directory: C:/Users/v48305jm/System Files/PhD Computer Science/Code/Epydex/Epydex
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2024.1.1/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/v48305jm/AppData/Local/JetBrains/CLion 2024.1.2/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "E:/Home Desktop Files/Work/University of Manchester/PhD Work/Gitlab Work/Epydex/Epydex/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/v48305jm/System Files/PhD Computer Science/Code/Epydex/Epydex/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
